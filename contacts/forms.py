9e310a65499193269595a319ca3246d6ab0f70a6
MokMay <mokhtarimay@gmail.com>
2026-07-24 23:09:56 +0100

    Add email and status as details with the contact

--- contacts/forms.py
+++ contacts/forms.py
@@ -4,4 +4,4 @@ from .models import Contact
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ["name", "phone_number"]
+        fields = ["name", "phone_number","email","status"]
--- contacts/models.py
+++ contacts/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20)
+    email = models.EmailField(blank=True)
+    status = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return self.name
