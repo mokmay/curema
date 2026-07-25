fdaa742b80a9336ef4e55525d4f91af52758a98c
MokMay <mokhtarimay@gmail.com>
2026-07-25 12:08:28 +0100

    replace text with status options for the status field

--- contacts/forms.py
+++ contacts/forms.py
@@ -5,3 +5,9 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ["name", "phone_number","email","status"]
+
+        widgets = {
+            "status": forms.Select(attrs={
+                "class": "form-select"
+            }),
+        }
--- contacts/models.py
+++ contacts/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 class Contact(models.Model):
+    STATUS_CHOICES = [
+        ("not_contacted", "Not Contacted"),
+        ("contacted", "Contacted"),
+        ("responded", "Responded"),
+        ("done", "Done"),
+    ]
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20)
     email = models.EmailField(blank=True)
     status = models.CharField(max_length=50, blank=True)
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="not_contacted"
+    )
 
     def __str__(self):
         return self.name
